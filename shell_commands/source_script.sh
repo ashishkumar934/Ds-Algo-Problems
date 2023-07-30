4af0e950e05c322a98d9ca5179de3f8381be173a
Ashish Kumar <ak1997934@gmail.com>
2023-07-30 17:50:44 +0530

    Added the shell script code

--- shell_commands/source_script.sh
+++ shell_commands/source_script.sh
@@ -1,4 +1,11 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 echo "Hello"
-MY_NAME='ashish kumar singh'
+export MY_NAME='ashish kumar singh'
+set -a
+var1='krishna'
+sh called_script_1.sh ## - echo is not done. When export is used echo is done.
+#source called_script_1.sh ## - echo is done
+#./called_script_1.sh ## running like this permission denied. With export echo is done
+#. ./called_script_1.sh ## - echo is done
+#export MY_NAME
